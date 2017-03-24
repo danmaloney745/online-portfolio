650e00e35097b7731fdc8667354d5d1a992c860e
G00303381 <danielmaloneygti@gmail.com>
2017-03-24 03:36:34 +0000

    Added smooth scroll

--- public/js/init.js
+++ public/js/init.js
@@ -2,6 +2,12 @@
   $(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
+    $('.scrollspy').click(function (e) {
+      let linkHref = $(this).attr('href');
+      $('html, body').animate({
+        scrollTop: $(linkHref).offset().top - 50
+      }, 1000);
+      e.preventDefault(); //removes section name from url
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
